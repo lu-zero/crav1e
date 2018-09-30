a6901b0f553ce87c72f105c7513fa880a92c3c78
Luca Barbato <lu_zero@gentoo.org>
2018-09-30 16:02:12 +0200

    First iteration
    It exposes the current rav1e basic api.

---
+++ README.md
@@ -0,0 +1,15 @@
+# C rav1e API
+
+[cbindgen]()-based API for C/C++ users.
+
+## Status
+
+The API is far from stable
+
+- [x] Basic encoding usage
+- [ ] mkv/mp4 support
+  - [ ] OBU Sequence Header
+  - [ ] TimeInfo
+- [ ] Documentation
+- [ ] Examples
+- [ ] Install target
---
+++ build.rs
@@ -0,0 +1,17 @@
+extern crate cbindgen;
+
+fn main() {
+    let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    cbindgen::Builder::new()
+        .with_crate(crate_dir)
+        .with_parse_deps(true)
+        .with_header("")
+        .with_include_guard("RAV1E_H")
+        .with_language(cbindgen::Language::C)
+        .generate()
+        .expect("Cannot generate bindings")
+        .write_to_file("rav1e.h");
+
+    println!("cargo:rerun-if-changed=src/lib.rs");
+}

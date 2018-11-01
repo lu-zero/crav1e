9d7e61d526d5b40fb6bd42c5a9013a7ad77bf95f
Luca Barbato <lu_zero@gentoo.org>
2018-11-02 00:57:55 +0100

    Remove commented code

--- build.rs
+++ build.rs
@@ -3,16 +3,7 @@ extern crate cbindgen;
 fn main() {
     let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let header_path: std::path::PathBuf = ["include", "rav1e.h"].iter().collect();
-/*
-    cbindgen::Builder::new()
-        .with_crate(crate_dir)
-        .with_parse_deps(true)
-        .with_header("// SPDX-License-Identifier: MIT")
-        .with_include_guard("RAV1E_H")
-        .with_language(cbindgen::Language::C)
-        .generate()
-        .expect("Cannot generate bindings")
-*/
+
     cbindgen::generate(crate_dir).unwrap().write_to_file(header_path);
 
     println!("cargo:rerun-if-changed=src/lib.rs");

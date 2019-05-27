8b9e5bcb73f5786279fe695b491432606c5b2ded
Luca Barbato <lu_zero@gentoo.org>
2019-05-27 20:05:14 +0200

    Initial support for non-host targets

--- build.rs
+++ build.rs
@@ -3,6 +3,11 @@ extern crate cbindgen;
 
 // TODO move to a separate crate and/or cargo-c to not repeat yourself.
 fn shared_object_link_line() {
+    use std::env;
+    // let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let env = env::var("CARGO_CFG_TARGET_ENV").unwrap();
+
     // We do not care about `_pre` and such.
     let major = env!("CARGO_PKG_VERSION_MAJOR");
     let minor = env!("CARGO_PKG_VERSION_MINOR");
@@ -10,13 +15,13 @@ fn shared_object_link_line() {
 
     let link = "cargo:rustc-cdylib-link-arg=";
 
-    if cfg!(target_os = "linux") {
+    if os == "linux" {
         println!("{}-Wl,-soname,librav1e.so.{}", link, major);
-    } else if cfg!(target_os = "macos") {
+    } else if os == "macos" {
         let libdir = "/usr/local/lib";
         println!("{0}-Wl,-install_name,{1}/librav1e.{2}.{3}.{4}.dylib,-current_version,{2}.{3}.{4},-compatibility_version,{2}",
                 link, libdir, major, minor, micro);
-    } else if cfg!(target_os = "windows") {
+    } else if os == "windows" && env == "gnu" {
         // This is only set up to work on GNU toolchain versions of Rust
         let out_dir = std::path::PathBuf::from("target").join(std::env::var("PROFILE").unwrap());
 
